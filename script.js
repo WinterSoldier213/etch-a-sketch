d9838dbe89867d3d59bcd12cb7b641249db981be
WinterSoldier213 <armanbance@gmail.com>
2024-04-20 13:11:29 -0700

    etch a sketch whole project

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Anakin has crazy aura")
+const container = document.querySelector(".container");
+
+for (let i=0; i<16; i++) {
+    for (let i=0; i<16; i++) {
+        const div = document.createElement("div");
+        div.classList="box";
+        container.appendChild(div);
+    }
+}
+const boxes = document.querySelectorAll(".box");
+boxes.forEach(box => {
+    box.addEventListener("mouseover", ()=> {
+        box.style.backgroundColor="#ccc";
+    });
+    box.addEventListener("mouseout", () => {
+        box.style.backgroundColor="transparent";
+    })
+})
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    const yo = prompt("Number of squares per side???")
+})
